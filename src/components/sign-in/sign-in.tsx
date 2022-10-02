56918f0041c8099fcc401c26b44c62524eb119d4
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-10-02 17:35:33 +0300

    Sign Up page

--- src/components/sign-in/sign-in.tsx
+++ src/components/sign-in/sign-in.tsx
@@ -7,11 +7,11 @@ import {AppRoutes} from "../../common/enums/enums";
 const SignIn: FC = () => {
     return <div className={styles.signInWrapper}>
         <form className={styles.signInForm}>
-            <h3 className={styles.formHeading}>Увійти</h3>
+            <h3 className={styles.formHeading}>Вхід</h3>
             <label className={styles.formLabel}>Email</label>
-            <input placeholder="Your email" type="email" name="email"/>
-            <label className={styles.formLabel}>Password</label>
-            <input placeholder="Your password" type="password" name="password"/>
+            <input placeholder="Ваш email" type="email" name="email"/>
+            <label className={styles.formLabel}>Пароль</label>
+            <input placeholder="Ваш пароль" type="password" name="password"/>
             <button className={styles.signInButton} type="submit">Увійти</button>
             <div className={styles.switchWrapper}>
                 <p className={styles.switch}>Не маєте акаунту?</p>
--- src/components/sign-up/sign-up.tsx
+++ src/components/sign-up/sign-up.tsx
@@ -1,23 +1,37 @@
 import {FC} from "react";
 
 import styles from './styles.module.css'
+import {Link} from "react-router-dom";
+import {AppRoutes} from "../../common/enums/app/app-route.enum";
 
 const SignUp: FC = () => {
     return <div className={styles.signUpWrapper}>
         <form className={styles.signUpForm}>
-            <input type="email" name="email"/>
-            <input type="text" name="name"/>
-            <input type="text" name="surname"/>
-            <input type="text" name="secondName"/>
-            <input type="date" name="birthDate"/>
+            <h3 className={styles.formHeading}>Реєстрація</h3>
+            <label className={styles.formLabel}>Email</label>
+            <input placeholder="Ваш email" type="email" name="email"/>
+            <label className={styles.formLabel}>Ім'я</label>
+            <input placeholder="Ваше ім'я" type="text" name="name"/>
+            <label className={styles.formLabel}>Прізвище</label>
+            <input placeholder="Ваше прізвище" type="text" name="surname"/>
+            <label className={styles.formLabel}>По-батькові</label>
+            <input placeholder="Ваше по-батькові"  type="text" name="secondName"/>
+            <label className={styles.formLabel}>Дата народження</label>
+            <input placeholder="Ваша дата народження"  type="date" name="birthDate"/>
+            <label className={styles.formLabel}>Оберіть вашу групу</label>
             <select name="group">
                 <option>IA-11</option>
                 <option>IA-12</option>
                 <option>IA-13</option>
                 <option>IA-14</option>
             </select>
-            <input type="password" name="password"/>
-            <button type="submit"/>
+            <label className={styles.formLabel}>Пароль</label>
+            <input placeholder="Ваш пароль" type="password" name="password"/>
+            <button className={styles.signUpButton} type="submit">Зареєструватись</button>
+            <div className={styles.switchWrapper}>
+                <p className={styles.switch}>Вже маєте акаунт?</p>
+                <button type="button"><Link to={AppRoutes.signIn}>Вхід</Link></button>
+            </div>
         </form>
     </div>
 }
