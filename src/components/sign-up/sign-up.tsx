62c91fb36d6cbc675c3095338823e7b6160e840c
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-10-16 23:10:47 +0300

    Table added

--- src/components/sign-up/sign-up.tsx
+++ src/components/sign-up/sign-up.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, FC, useState} from "react";
+import {ChangeEvent, FC, FormEvent, useMemo, useState} from "react";
 import {Link} from "react-router-dom";
 
 import styles from './styles.module.css'
@@ -11,9 +11,24 @@ import {SignUpValidator} from "../../validation/sign-up/sign-up.validator";
 
 const SignUp: FC = () => {
     const validator = new SignUpValidator();
+    const [tableData, setTableData] = useState<UserRegisterData[]>([]);
     const [userData, setUserData] = useState<UserRegisterData>(defaultUserDataValues);
     const [errors, setErrors] = useState<RegistrationErrors>(defaultErrorsValues);
     const [isRegistrationLocked, setIsRegistrationLocked] = useState<boolean>(true);
+
+    const resetData = () => {
+        setUserData({
+            email: '',
+            phoneNumber: '',
+            name: '',
+            surname: '',
+            secondName: '',
+            gender: '',
+            group: 'IA-11',
+            birthDate: '',
+            password: ''
+        });
+    }
 
     const handleChangeFormState = (event: ChangeEvent<HTMLInputElement>): void => {
         setUserData(prevState => {
@@ -46,43 +61,58 @@ const SignUp: FC = () => {
         });
     }
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setTableData(prevState => ([...prevState, userData]))
+        resetData();
+    }
+
 
     return <div className={styles.signUpWrapper}>
-        <form className={styles.signUpForm}>
+        <form className={styles.signUpForm} onSubmit={handleSubmit}>
             <h3 className={styles.formHeading}>Реєстрація</h3>
             <label className={styles.formLabel}>Email</label>
-            <input placeholder="Ваш email" type="email" name="email" onChange={handleChangeFormState} value={userData.email}/>
+            <input placeholder="Ваш email" type="email" name="email" onChange={handleChangeFormState}
+                   value={userData.email}/>
             <label className={styles.errorLabel}>{errors.email}</label>
             <label className={styles.formLabel}>Номер телефону</label>
-            <input placeholder="+38(0__)-___-__-__" type="tel" name="phoneNumber" onChange={handleChangeFormState} value={userData.phoneNumber}/>
+            <input placeholder="+38(0__)-___-__-__" type="tel" name="phoneNumber" onChange={handleChangeFormState}
+                   value={userData.phoneNumber}/>
             <label className={styles.errorLabel}>{errors.phoneNumber}</label>
             <label className={styles.formLabel}>Ім'я</label>
-            <input placeholder="Ваше ім'я" type="text" name="name" onChange={handleChangeFormState} value={userData.name}/>
+            <input placeholder="Ваше ім'я" type="text" name="name" onChange={handleChangeFormState}
+                   value={userData.name}/>
             <label className={styles.errorLabel}>{errors.name}</label>
             <label className={styles.formLabel}>Прізвище</label>
-            <input placeholder="Ваше прізвище" type="text" name="surname" onChange={handleChangeFormState} value={userData.surname}/>
+            <input placeholder="Ваше прізвище" type="text" name="surname" onChange={handleChangeFormState}
+                   value={userData.surname}/>
             <label className={styles.errorLabel}>{errors.surname}</label>
             <label className={styles.formLabel}>По-батькові</label>
-            <input placeholder="Ваше по-батькові"  type="text" name="secondName" onChange={handleChangeFormState} value={userData.secondName}/>
+            <input placeholder="Ваше по-батькові" type="text" name="secondName" onChange={handleChangeFormState}
+                   value={userData.secondName}/>
             <label className={styles.errorLabel}>{errors.secondName}</label>
             <ul className={styles.genderSection}>
                 <label className={styles.formLabel}>Стать</label>
                 <div className={styles.genderSectionFraction}>
-                    <input placeholder="Ваше по-батькові"  type="radio" value="male" name="gender" onChange={handleChangeFormState}/>
+                    <input placeholder="Ваше по-батькові" type="radio" value="male" name="gender"
+                           onChange={handleChangeFormState}/>
                     <label className={styles.genderOptionLabel}>Чоловіча</label>
                 </div>
                 <div className={styles.genderSectionFraction}>
-                    <input placeholder="Ваше по-батькові"  type="radio" value="female" name="gender" onChange={handleChangeFormState}/>
+                    <input placeholder="Ваше по-батькові" type="radio" value="female" name="gender"
+                           onChange={handleChangeFormState}/>
                     <label className={styles.genderOptionLabel}>Жіноча</label>
                 </div>
                 <div className={styles.genderSectionFraction}>
-                    <input placeholder="Ваше по-батькові"  type="radio" value="other" name="gender" onChange={handleChangeFormState}/>
+                    <input placeholder="Ваше по-батькові" type="radio" value="other" name="gender"
+                           onChange={handleChangeFormState}/>
                     <label className={styles.genderOptionLabel}>Інше</label>
                 </div>
             </ul>
             <label className={styles.errorLabel}>{errors.gender}</label>
             <label className={styles.formLabel}>Дата народження</label>
-            <input placeholder="Ваша дата народження"  type="date" name="birthDate" onChange={handleChangeFormState} value={userData.birthDate}/>
+            <input placeholder="Ваша дата народження" type="date" name="birthDate" onChange={handleChangeFormState}
+                   value={userData.birthDate}/>
             <label className={styles.errorLabel}>{errors.birthDate}</label>
             <label className={styles.formLabel}>Оберіть вашу групу</label>
             <select name="group" onChange={handleChangeGroup} value={userData.group}>
@@ -93,14 +123,57 @@ const SignUp: FC = () => {
             </select>
             <label className={styles.errorLabel}>{errors.group}</label>
             <label className={styles.formLabel}>Пароль</label>
-            <input placeholder="Ваш пароль" type="password" name="password" onChange={handleChangeFormState} value={userData.password}/>
+            <input placeholder="Ваш пароль" type="password" name="password" onChange={handleChangeFormState}
+                   value={userData.password}/>
             <label className={styles.errorLabel}>{errors.password}</label>
-            <button className={`${styles.signUpButton} ${isRegistrationLocked && styles.locked}`} disabled={isRegistrationLocked} type="submit">Зареєструватись</button>
+            <button className={`${styles.signUpButton} ${isRegistrationLocked && styles.locked}`}
+                    disabled={isRegistrationLocked} type="submit">Зареєструватись
+            </button>
             <div className={styles.switchWrapper}>
                 <p className={styles.switch}>Вже маєте акаунт?</p>
-                <Link to={AppRoutes.signIn}><button type="button">Вхід</button></Link>
+                <Link to={AppRoutes.signIn}>
+                    <button type="button">Вхід</button>
+                </Link>
             </div>
         </form>
+        <table className={styles.dataTable} border={1} rules={"rows"}>
+            <thead>
+            <tr>
+                <th>Email</th>
+                <th>Ім'я</th>
+                <th>Прізвище</th>
+                <th>По-батькові</th>
+                <th>Група</th>
+                <th>Дата народження</th>
+                <th>Номер телефону</th>
+                <th>Пароль</th>
+                <th>Стать</th>
+            </tr>
+            </thead>
+            <tbody>
+            {tableData.map(({
+                                email,
+                                name,
+                                surname,
+                                secondName,
+                                group,
+                                birthDate,
+                                phoneNumber,
+                                password,
+                                gender
+                            }, index) => <tr key={index}>
+                <td>{email}</td>
+                <td>{name}</td>
+                <td>{surname}</td>
+                <td>{secondName}</td>
+                <td>{group}</td>
+                <td>{birthDate}</td>
+                <td>{phoneNumber}</td>
+                <td>{password}</td>
+                <td>{gender}</td>
+            </tr>)}
+            </tbody>
+        </table>
     </div>
 }
 
